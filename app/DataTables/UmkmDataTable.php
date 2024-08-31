fcb08928b58838fe6bbcd6575c7b356bea0464c6
PramAhmad <pramuditaadit2810@gmail.com>
2024-09-01 00:43:20 +0700

    add search in umkm

--- app/DataTables/UmkmDataTable.php
+++ app/DataTables/UmkmDataTable.php
@@ -8,14 +8,13 @@ use Yajra\DataTables\EloquentDataTable;
 use Yajra\DataTables\Html\Builder as HtmlBuilder;
 use Yajra\DataTables\Html\Button;
 use Yajra\DataTables\Html\Column;
-use Yajra\DataTables\Html\Editor\Editor;
-use Yajra\DataTables\Html\Editor\Fields;
 use Yajra\DataTables\Services\DataTable;
 use Yajra\DataTables\WithExportQueue;
 
 class UmkmDataTable extends DataTable
 {
-  use WithExportQueue;
+    use WithExportQueue;
+
     /**
      * Build the DataTable class.
      *
@@ -24,41 +23,40 @@ class UmkmDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-           
             ->editColumn('user_id', function ($umkm) {
                 return $umkm->nama;
             })
-
             ->editColumn('approved', function ($umkm) {
-              return $umkm->approved ? 
-                  '<span class="badge font-medium bg-light-primary text-primary">Approved</span>' : 
-                  '<span class="badge font-medium bg-light-danger text-danger">Not Approved</span>';
-          })
-          ->editColumn('action', function($umkm){
-            return '<div class="btn-group">
-                      <button
-                        class="btn btn-light-secondary text-secondary font-medium btn-sm dropdown-toggle"
-                        type="button"
-                        data-bs-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                      >
-                        Actions
-                      </button>
-                      <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/admin/umkm/show/'.$umkm->id.'">Detail</a></li>
-                      
-                         <li>
-                    <button type="button" class="dropdown-item" onclick="confirmDelete('.$umkm->id.')" >Delete</button>
-                </li>
-                      </ul>
-                    </div>';
-        })
-        
-          
+                return $umkm->approved 
+                    ? '<span class="badge font-medium bg-light-primary text-primary">Approved</span>' 
+                    : '<span class="badge font-medium bg-light-danger text-danger">Not Approved</span>';
+            })
+            ->addColumn('action', function($umkm) {
+                return '<div class="btn-group">
+                            <button
+                                class="btn btn-light-secondary text-secondary font-medium btn-sm dropdown-toggle"
+                                type="button"
+                                data-bs-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                Actions
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="/admin/umkm/show/'.$umkm->id.'">Detail</a></li>
+                                <li>
+                                    <button type="button" class="dropdown-item" onclick="confirmDelete('.$umkm->id.')">Delete</button>
+                                </li>
+                            </ul>
+                        </div>';
+            })
+            ->filterColumn('user_id', function($query, $keyword) {
+                $query->whereHas('user', function($query) use ($keyword) {
+                    $query->where('nama', 'like', "%{$keyword}%");
+                });
+            })
             ->setRowId('id')
-            ->rawColumns(['action','approved']);
-
+            ->rawColumns(['action', 'approved']);
     }
 
     /**
@@ -66,7 +64,7 @@ class UmkmDataTable extends DataTable
      */
     public function query(Umkm $model): QueryBuilder
     {
-        return $model->newQuery()->where('approved', "!=", 2);
+        return $model->newQuery()->with('user')->where('approved', '!=', 2);
     }
 
     /**
@@ -75,22 +73,20 @@ class UmkmDataTable extends DataTable
     public function html(): HtmlBuilder
     {
         return $this->builder()
-                    ->setTableId('umkm')
-                    ->columns($this->getColumns())
-                    ->minifiedAjax()
-                    ->searching()
-                    //->dom('Bfrtip')
-                    ->orderBy(1)
-                  
-                    ->selectStyleSingle()
-                    ->buttons([
-                        Button::make('excel'),
-                        Button::make('csv'),
-                        Button::make('pdf'),
-                        Button::make('print'),
-                        Button::make('reset'),
-                        Button::make('reload')
-                    ]);
+            ->setTableId('umkm')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->searching()
+            ->orderBy(1)
+            ->selectStyleSingle()
+            ->buttons([
+                Button::make('excel'),
+                Button::make('csv'),
+                Button::make('pdf'),
+                Button::make('print'),
+                Button::make('reset'),
+                Button::make('reload')
+            ]);
     }
 
     /**
@@ -100,17 +96,22 @@ class UmkmDataTable extends DataTable
     {
         return [
             Column::computed('id')
-                  ->exportable(true)
-                  ->printable(false)
-                  ->width(60)
-                  ->addClass('text-center w-full '),
-              
-                Column::make('user_id'),
-                Column::make('nik'),
-                Column::make('nohp'),
-                Column::make('approved'),
-                Column::make('action')->exportable(false)->printable(false)->width(100)->addClass('text-center w-full '),
-
+                ->exportable(true)
+                ->printable(false)
+                ->width(60)
+                ->addClass('text-center w-full'),
+            Column::make('user_id')
+                ->title('Nama')
+                ->data('nama'),
+            Column::make('nik'),
+            Column::make('nohp'),
+            Column::make('approved'),
+            Column::make('action')
+                ->exportable(false)
+                ->printable(false)
+                ->searchable(false)
+                ->width(100)
+                ->addClass('text-center w-full'),
         ];
     }
 
